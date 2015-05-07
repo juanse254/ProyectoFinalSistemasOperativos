2c25bc9e220ce7e71d6f1f9b0dcef118264a318d
juanse254 <juanse254@gmail.com>
2015-05-07 18:27:36 -0500

    Push Original
    Codigo Fuente Original

---
+++ ProyectoFinalSO/src/proyectofinalso/Count.java
@@ -0,0 +1,20 @@
+
+
+/** A very tiny example MiniKernel program.
+ * Count down from 10 to 1.
+ * @see Kernel
+ */
+public class Count {
+    /** No public instances. */
+    private Count() {}
+
+    /** The main program.
+     * @param args ignored.
+     */
+    public static void main(String args[]) {
+        for (int i = 10; i > 0; i--) {
+            Library.output("Counting: " + i + "\n");
+        }
+        Library.output("*** Blast off ***!\n");
+    }
+}
---
+++ ProyectoFinalSO/src/proyectofinalso/FileSys.java
@@ -0,0 +1,23 @@
+
+
+import java.util.*;
+import java.io.*;
+import static java.lang.System.*;
+
+/** A file system. */
+
+public class FileSys {
+    /** The disk holding this file system. */
+    private FastDisk disk;
+
+
+    /** Initializes a FileSys instance for managing a disk.
+     *
+     * @param disk the disk containing the persistent data.
+     */
+    public FileSys(FastDisk disk) {
+        this.disk = disk;
+    } // FileSys(FastDisk)
+
+
+} // FileSys
