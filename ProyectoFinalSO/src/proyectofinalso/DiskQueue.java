0f406c9282a8514336e63cdfc99969e29af9623e
Carolina Herrera <carolinaherreracervantes1993@gmail.com>
2015-05-12 18:49:10 -0500

    Limpieza de codigo de metonos no utilizados (anylock)

--- ProyectoFinalSO/src/proyectofinalso/DiskQueue.java
+++ ProyectoFinalSO/src/proyectofinalso/DiskQueue.java
@@ -10,105 +10,87 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.locks.Lock;
+
 /**
  *
  * @author Sebastian
  */
 public class DiskQueue {
-    
+
     private static Disk disk;
     private boolean status; // if busy, true else false.
     private List<Request> queue;
 
-    public DiskQueue(Disk a){
+    public DiskQueue(Disk a) {
         disk = a;
-        status=false;
-        queue= new ArrayList<Request>();
+        status = false;
+        queue = new ArrayList<Request>();
     }
-    
+
     /**
-     * If the Resource(Disk) is not busy, calls the beginRead method on the disk to read the data on the block number.
+     * If the Resource(Disk) is not busy, calls the beginRead method on the disk
+     * to read the data on the block number.
      */
-     public  synchronized void read(int blocknumber, byte data[]) throws InterruptedException{
-         Request x = new Request(blocknumber, data, Request.task.READ);
-         queue.add(x);
-         Collections.sort(queue, new Comparator<Request>() {
-             @Override
-             public int compare(Request request1, Request request2) {
-                 return request1.getRequestBlock()-request2.getRequestBlock();
-             }
-         });
-         ProcessIO();
-         
+    public synchronized void read(int blocknumber, byte data[]) throws InterruptedException {
+        Request x = new Request(blocknumber, data, Request.task.READ);
+        queue.add(x);
+        Collections.sort(queue, new Comparator<Request>() {
+            @Override
+            public int compare(Request request1, Request request2) {
+                return request1.getRequestBlock() - request2.getRequestBlock();
+            }
+        });
+        ProcessIO();
+
     }
-     
-      /**
-     * If the Resource(Disk) is not busy, calls the writeRead method on the disk to read the data on the block number.
+
+    /**
+     * If the Resource(Disk) is not busy, calls the writeRead method on the disk
+     * to read the data on the block number.
      */
-     public  synchronized void write(int blocknumber, byte data[]) throws InterruptedException{    
-     
-     Request x = new Request(blocknumber, data, Request.task.WRITE);
-         queue.add(x);
-         Collections.sort(queue, new Comparator<Request>() {
-             @Override
-             public int compare(Request request1, Request request2) {
-                 return request1.getRequestBlock()-request2.getRequestBlock();
-             }
-         });
-         ProcessIO();
-         /*
-         while (status == true) {
-             wait();
-         }
-         status = true;
-         disk.beginWrite(blocknumber, data);
-     }
-     *
+    public synchronized void write(int blocknumber, byte data[]) throws InterruptedException {
+
+        Request x = new Request(blocknumber, data, Request.task.WRITE);
+        queue.add(x);
+        Collections.sort(queue, new Comparator<Request>() {
+            @Override
+            public int compare(Request request1, Request request2) {
+                return request1.getRequestBlock() - request2.getRequestBlock();
+            }
+        });
+        ProcessIO();
+
+    }
+
+    /**
+     * If the Resource(Disk) is not busy, calls the beginRead method on the disk
+     * to read the data on the block number.
      */
-     }
-         
-      /**
-     * If the Resource(Disk) is not busy, calls the beginRead method on the disk to read the data on the block number.
-     */
-     public synchronized void endIO(){
-         status=false;
-         notifyAll();
-         
-         
-     }
-     
-     public synchronized void ProcessIO() throws InterruptedException{
-         for(Request current:queue){
-             if(status){ 
-               wait();
-             } 
-             
-             else if(current.getRequestTask() == Request.task.READ){
-                 status=true;
-                 disk.beginRead(current.getRequestBlock(), current.getRequestBuffer());
-                 
-                         
-                         
-                 //queue.remove(current); 
-             }
-             else if(current.getRequestTask() == Request.task.WRITE){
-                 status=true;
-                 disk.beginWrite(current.getRequestBlock(), current.getRequestBuffer());
-                 //current.notifyRequest();
-                 //queue.remove(current); 
-             }
-             
-            
-         }
-     }
-     
-     private synchronized boolean AnyLock(){
-         for(Request current:queue){
-             if(current.getisLocked())
-                 System.out.println(current.getisLocked());
-                 return true;
-         }
-         return false;
-     }
-    
+    public synchronized void endIO() {
+        status = false;
+        notifyAll();
+
+    }
+
+    public synchronized void ProcessIO() throws InterruptedException {
+        for (Request current : queue) {
+            if (status) {
+                wait();
+            } else if (current.getRequestTask() == Request.task.READ) {
+                status = true;
+                disk.beginRead(current.getRequestBlock(), current.getRequestBuffer());
+                current.setTaskNULL();
+
+            } else if (current.getRequestTask() == Request.task.WRITE) {
+                status = true;
+                disk.beginWrite(current.getRequestBlock(), current.getRequestBuffer());
+                current.setTaskNULL();
+
+            } else if (current.getRequestTask() == Request.task.NULL) {
+
+            }
+
+        }
+    }
+
 }
