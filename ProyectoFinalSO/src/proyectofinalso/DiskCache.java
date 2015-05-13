32fba4cf713bb39c9785ec7c5bdb8fc3cdaaecf7
Carolina Herrera <carolinaherreracervantes1993@gmail.com>
2015-05-12 20:15:33 -0500

    Creacion de la clase Diskcache

---
+++ ProyectoFinalSO/src/proyectofinalso/DiskCache.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinalso;
+
+/**
+ *
+ * @author user
+ */
+public class DiskCache {
+
+    public synchronized void read() {
+
+    }
+
+    public synchronized void write() {
+
+    }
+
+    public void shutDown() {
+
+    }
+}
